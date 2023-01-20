3f96e5abeaa8bf2c1468d6583ebfaade78474e8a
gdulus <gdula.pawel@gmail.com>
2023-01-20 18:21:30 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM asyncapi/generator
+
+RUN npm install express --save
+
+COPY ./app.js /app/app.js
+
+ENTRYPOINT node /app/app.js
---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const { exec } = require("child_process");
+const fs = require('fs');
+
+const app = express()
+const port = 3000
+
+app.get('/generator/index-page', (req, res) => {
+    var src = req.query.src
+    var id = req.query.id
+
+    if (!src || !id){
+        res.status(400);
+        res.json({ status: 'errors', code: 'validation.failed'})
+        return
+    }
+
+    var resultDir = "asynaAPI-" + id
+    var command = "ag " + src + " @asyncapi/html-template  -o " + resultDir
+
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            res.status(500);
+            res.json({ status: 'errors', code: 'cmd.error', result: error })
+            return;
+        }
+        if (stderr) {
+            res.status(500);
+            res.json({ status: 'errors', code: 'cmd.error', result: stderr })
+            return;
+        }
+
+        var outputDir = '/app/' + resultDir;
+
+        fs.readFile(outputDir + '/index.html', 'utf8', (err, data) => {
+          if (err) {
+            res.status(500);
+            res.json({ status: 'errors', code: 'fileRead.error', result: err })
+            return;
+          }
+          fs.rmSync(outputDir, { recursive: true, force: true });
+          res.status(200);
+          res.json({ status: 'success', result: data })
+        });
+
+    });
+})
+
+app.listen(port, () => {
+  console.log("Starting AsyncAPI generator on port " + port)
+})
