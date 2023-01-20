64491431d204c4ac16053a386cbf768caa7532a8
gdulus <gdula.pawel@gmail.com>
2023-01-20 18:46:16 +0100

    changing api structure

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { exec } = require("child_process");
 const fs = require('fs');
+var crypto = require('crypto');
 
 const app = express()
 const port = 3000
@@ -13,13 +14,14 @@ process.on('SIGINT', function() {
 
 app.get('/generator/index-page', (req, res) => {
     var src = req.query.src
-    var id = req.query.id
 
-    if (!src || !id){
+    if (!src){
         res.status(400);
-        res.json({ status: 'errors', code: 'validation.failed'})
+        res.json({ error: 'validation.src.notEmpty'})
         return
     }
+
+    var id = crypto.createHash('md5').update(src).digest("hex")
 
     var resultDir = "asynaAPI-" + id
     var command = "ag " + src + " @asyncapi/html-template  -o " + resultDir
@@ -27,12 +29,12 @@ app.get('/generator/index-page', (req, res) => {
     exec(command, (error, stdout, stderr) => {
         if (error) {
             res.status(500);
-            res.json({ status: 'errors', code: 'cmd.error', result: error })
+            res.json({ error: 'cmd.error', message: error })
             return;
         }
         if (stderr) {
             res.status(500);
-            res.json({ status: 'errors', code: 'cmd.error', result: stderr })
+            res.json({ error: 'cmd.error', message: stderr })
             return;
         }
 
@@ -41,12 +43,12 @@ app.get('/generator/index-page', (req, res) => {
         fs.readFile(outputDir + '/index.html', 'utf8', (err, data) => {
           if (err) {
             res.status(500);
-            res.json({ status: 'errors', code: 'fileRead.error', result: err })
+            res.json({ error: 'fileRead.error', message: err })
             return;
           }
           fs.rmSync(outputDir, { recursive: true, force: true });
           res.status(200);
-          res.json({ status: 'success', result: data })
+          res.json({ id: id, result: data })
         });
 
     });
